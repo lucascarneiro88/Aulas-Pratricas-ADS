7bb53902c943c1ebdd14fbe811d351fc3f73f171
Lucas <carneirolucas88@gmail.com>
2024-06-21 16:13:15 -0300

    explicando help(print)

---
+++ aula_pratica4/help_print.py
@@ -0,0 +1,7 @@
+help(print) 
+# usado para exibir informações detalhadas sobre a função print. 
+#Quando você executa esse comando, o interpretador de Python exibe a documentação da função print,
+# que inclui uma descrição de seu propósito, os argumentos que ela aceita
+#No caso do pycharm executa também no terminal de comandos
+
+
