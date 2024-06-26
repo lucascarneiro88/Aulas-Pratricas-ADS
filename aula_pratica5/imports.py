98e6b8b5689bdb7f19a4281f4dbc96f438d5ccc8
Lucas <carneirolucas88@gmail.com>
2024-06-26 12:04:42 -0300

    importando biliotecas

---
+++ aula_pratica5/imports.py
@@ -0,0 +1,13 @@
+
+# IMPORTANDO BIBLIOTECA MATH
+# import math
+# print(math.sqrt(9))
+
+# RENOMEANDO OU DANDO APELIDO PARA BIBLIOTECA
+# import math as m
+# print(m.sqrt(9))
+
+# IMPORTANDO SOMENTE A FUNÇÃO, MÉTODO DESEJADO DENTRO DA BIBLIOTECA
+#mais indicado
+from math import sqrt
+print(sqrt(9))
